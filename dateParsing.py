2b742aeba5d8b0f21c3d221894815dc96dbf5c40
marunemo <chys3697@gmail.com>
2021-10-08 16:56:32 +0900

    Parsing every opening hours

--- dateParsing.py
+++ dateParsing.py
@@ -7,18 +7,40 @@ with open('dayOff.txt') as openingHour:
         dateData = dateData.strip(" \n&").split(" & ")
         weekHours = {i : [[]] for i in week}
         for hours in dateData:
-            if hours.startswith('매일'):
-                if '브레이크타임' in hours:
-                    spliter = hours[hours.index(" ", hours.index("브레이크타임")) + 1:].split(" ~ ")
-                    for day in week:
-                        for i, openingHour in enumerate(weekHours[day]):
-                            if openingHour[1] > spliter[0] and openingHour[0] < spliter[0]:
-                                weekHours[day].append([spliter[1], openingHour[1]])
-                                weekHours[day][i][1] = spliter[0]
+            weekRange = hours[:hours.index(" ")]
+            if weekRange == "매일":
+                validWeek = week
+                hours = hours[hours.index(" ") + 1:]
+            elif "~" in weekRange:
+                validWeek = week[week.index(weekRange[0]):week.index(weekRange[2]) + 1]
+                hours = hours[hours.index(" ") + 1:]
+            elif len(weekRange) == 1:
+                index = 0
+                while not hours[index].isdigit():
+                    index += 1
+                validWeek = hours[:index].split()
+                hours = hours[index:]
+            else:
+                if weekRange == "휴무일:":
+                    weekHours = hours
                 else:
-                    for day in week:
-                        weekHours[day] = [hours[hours.index(" ") + 1:].split(" ~ ")]
-        for day in week:
-            dayWeek = sorted(weekHours[day])
-            result.write(day + " : " + str(dayWeek) + " / ")
-        result.write('\n')
+                    weekHours = "휴무일: " + hours
+                break
+
+            if '브레이크타임' in hours:
+                spliter = hours[hours.index(" ") + 1:].split(" ~ ")
+                for day in validWeek:
+                    for i, openingHour in enumerate(weekHours[day]):
+                        if openingHour[1] > spliter[0] and openingHour[0] < spliter[0]:
+                            weekHours[day].append([spliter[1], openingHour[1]])
+                            weekHours[day][i][1] = spliter[0]
+            else:
+                for day in validWeek:
+                    weekHours[day] = [hours.split(" ~ ")]
+        if type(weekHours) == str:
+            result.write(weekHours)
+        else:
+            for day in week:
+                dayWeek = sorted(weekHours[day])
+                result.write(day + " : " + str(dayWeek) + " / ")
+        result.write("\n")
