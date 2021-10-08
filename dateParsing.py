d5f07e381f8a24440f0eb09b1176da986a1fa508
marunemo <chys3697@gmail.com>
2021-10-08 16:29:17 +0900

    Parsing '매일'

--- dateParsing.py
+++ dateParsing.py
@@ -1,4 +1,24 @@
 with open('dayOff.txt') as openingHour:
     breaktime = openingHour.readlines()
+    result = open('parsingDate.txt', 'w')
 
-    
+    week = "월화수목금토일"
+    for dateData in breaktime:
+        dateData = dateData.strip(" \n&").split(" & ")
+        weekHours = {i : [[]] for i in week}
+        for hours in dateData:
+            if hours.startswith('매일'):
+                if '브레이크타임' in hours:
+                    spliter = hours[hours.index(" ", hours.index("브레이크타임")) + 1:].split(" ~ ")
+                    for day in week:
+                        for i, openingHour in enumerate(weekHours[day]):
+                            if openingHour[1] > spliter[0] and openingHour[0] < spliter[0]:
+                                weekHours[day].append([spliter[1], openingHour[1]])
+                                weekHours[day][i][1] = spliter[0]
+                else:
+                    for day in week:
+                        weekHours[day] = [hours[hours.index(" ") + 1:].split(" ~ ")]
+        for day in week:
+            dayWeek = sorted(weekHours[day])
+            result.write(day + " : " + str(dayWeek) + " / ")
+        result.write('\n')
