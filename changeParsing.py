c67ebbedf7f08ed1dd0ec657e746b5b3e08a75d1
marunemo <chys3697@gmail.com>
2021-11-12 17:53:33 +0900

    create: parsing by json to json

---
+++ changeParsing.py
@@ -0,0 +1,60 @@
+import json
+
+with open("hanchelin-guide-export.json") as jsonFile:
+    restData = json.load(jsonFile)
+    restList = restData["식당"]
+    result = open('hanchelin-guide-change.json', 'w', encoding='utf8')
+
+    for rest in restList:
+        try:
+            dateData = ""
+            if type(rest['opening_hours']) == list:
+                for hours in rest['opening_hours']:
+                    dateData += hours + ' & '
+                dateData += '\n'
+            else:
+                dateData + rest['opening_hours'] + '\n'
+            week = "월화수목금토일"
+            dateData = dateData.strip(" \n&").split(" & ")
+            weekHours = {i : [[]] for i in week}
+            for hours in dateData:
+                weekRange = hours[:hours.index(" ")]
+                if weekRange == "매일":
+                    validWeek = week
+                    hours = hours[hours.index(" ") + 1:]
+                elif "~" in weekRange:
+                    validWeek = week[week.index(weekRange[0]):week.index(weekRange[2]) + 1]
+                    hours = hours[hours.index(" ") + 1:]
+                elif len(weekRange) == 1:
+                    index = 0
+                    while not hours[index].isdigit():
+                        index += 1
+                    validWeek = hours[:index].split()
+                    hours = hours[index:]
+                else:
+                    if weekRange == "휴무일:":
+                        weekHours = hours
+                    else:
+                        weekHours = "휴무일: " + hours
+                    break
+
+                if '브레이크타임' in hours:
+                    spliter = hours[hours.index(" ") + 1:].split(" ~ ")
+                    for day in validWeek:
+                        for i, openingHour in enumerate(weekHours[day]):
+                            if openingHour[1] > spliter[0] and openingHour[0] < spliter[0]:
+                                weekHours[day].append([spliter[1], openingHour[1]])
+                                weekHours[day][i][1] = spliter[0]
+                else:
+                    for day in validWeek:
+                        weekHours[day] = [hours.split(" ~ ")]
+            if type(weekHours) == str:
+                rest['opening_hours'] = weekHours
+            else:
+                for day in week:
+                    dayWeek = sorted(weekHours[day])
+                    rest['opening_hours'] = day + " : " + str(dayWeek) + " / "
+            json.dump(rest, result)
+        except:
+            pass
+print(rest)
